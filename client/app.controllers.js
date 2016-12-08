af49f5dba17fa2d7f73b5af0a144e35c9f73a471
Weijian Zeng <weijian@prosparency.com>
2016-12-08 20:21:20 +0000

    add navbar

--- client/app.controllers.js
+++ client/app.controllers.js
@@ -2,11 +2,25 @@
  * Weijian Zeng
  */
 
+
+
+
 var homeCtrl = function($scope, $http, $stateParams, API ){
     var vm;
     vm = this;
     vm.title = 'Volunteering Opportunities';
     
+    vm.data = [];
+    
+    vm.data.push( { title: 'Volunteering Op 1',
+                 link: 'htttp:www.site1.com'
+            });
+    
+    vm.data.push( { title: 'Volunteering Op 2',
+                 link: 'htttp:www.site2.com'
+            });
+    
+    console.log(vm.data)
     console.log('controller');
     
 }
